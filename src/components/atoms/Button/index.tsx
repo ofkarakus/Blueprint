775a81e437d52bf5e45646ecb378028483da4aac
Omer Karakus <omer.karakus@lonres.com>
2022-09-30 15:59:58 +0100

    add toolbar icons

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,5 +1,8 @@
-const Button = () => {
-  return <div>Button</div>;
+import { ReactNode } from "react";
+import "./styles.css";
+
+const Button = ({ children }: { children?: ReactNode }) => {
+  return <div className="btn">{children}</div>;
 };
 
 export default Button;
